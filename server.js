676e26a646b2a46bd6dae27619706fb15310bb79
Beatriz <beatriz@fullcircle.es>
2019-04-15 11:38:40 +0200

    Initial server configurations

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongo = require('mongodb').MongoClient
+
+#Initialize Express App
+const app = express()
+
+#The stubs for the API endpoints:
+app.post('/trip', (req,res) => { /* */ })
+app.get('/trips', (req, res) => { /* */ })
+app.post('/expense', (req, res) => { /* */ })
+app.get('/expenses', (req, res) => { /* */ })
+
+#Uses listen() to start the server
+app.listen(3000, () => console.log('YAY, server ready!'))
