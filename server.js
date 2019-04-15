ba64c501acf145ad2e970e20d0194962f7809280
Beatriz <beatriz@fullcircle.es>
2019-04-15 12:28:06 +0200

    Comments fixed

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const mongo = require('mongodb').MongoClient
 
-#Initialize Express App
+/*Initialize Express App*/
 const app = express()
 
-#The stubs for the API endpoints:
-app.post('/trip', (req,res) => { /* */ })
+/*The stubs for the API endpoints:*/
+app.post('/trip', (req, res) => { /* */ })
 app.get('/trips', (req, res) => { /* */ })
 app.post('/expense', (req, res) => { /* */ })
 app.get('/expenses', (req, res) => { /* */ })
 
-#Uses listen() to start the server
+/*Uses listen() to start the server*/
 app.listen(3003, () => console.log('YAY, server ready!'))
